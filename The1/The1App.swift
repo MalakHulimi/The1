b937c6059d9a214e66c740e5c89d811bc979e081
MalakHulimi <malakhulimi@gmail.com>
2022-11-16 15:12:51 +0300

    Initial Commit

---
+++ The1/The1App.swift
@@ -0,0 +1,17 @@
+//
+//  The1App.swift
+//  The1
+//
+//  Created by Malak  on 22/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct The1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
